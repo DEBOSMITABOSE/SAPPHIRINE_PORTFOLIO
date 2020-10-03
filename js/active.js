dbd1dbfb5581ac572a6beadc559a1512aa74b216
DEBOSMITABOSE <60229413+DEBOSMITABOSE@users.noreply.github.com>
2020-10-03 20:47:29 +0530

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,130 @@
+(function ($) {
+    'use strict';
+
+   //var browserWindow = $(window);
+
+    // :: 8.0 CounterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    // :: 9.0 Progress Bar Active Code
+    if ($.fn.circleProgress) {
+        
+        $('#c1').circleProgress({
+          size: 200,
+        emptyFill: "rgba(0, 0, 0, .0)",
+      fill: 'yellow',
+     thickness: '5',
+         reverse: true,
+        });
+    
+    }
+    if ($.fn.circleProgress) {
+        
+           $('#c2').circleProgress({
+            
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    
+    }
+    if ($.fn.circleProgress) {
+        $('#c3').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c4').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+
+    if ($.fn.circleProgress) {
+        $('#c5').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c6').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c7').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c8').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+
+    if ($.fn.circleProgress) {
+        $('#c9').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c10').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c11').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+    if ($.fn.circleProgress) {
+        $('#c12').circleProgress({
+            size: 200,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: 'yellow',
+            thickness: '5',
+            reverse: true,
+        });
+    }
+})(jQuery);
